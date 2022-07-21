4439dfc8ea44f7fcc6dd39d651f5e28260ca1b4e
Bram Stoeller <bram.stoeller@alliander.com>
2022-07-21 15:16:45 +0200

    Initial commit
    Signed-off-by: Bram Stoeller <bram.stoeller@alliander.com>

---
+++ README.md
@@ -0,0 +1,6 @@
+<!--
+SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model IO project <dynamic.grid.calculation@alliander.com>
+
+SPDX-License-Identifier: MPL-2.0
+-->
+# Power Grid Model Input/Output
---
+++ setup.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model IO project <dynamic.grid.calculation@alliander.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from setuptools import setup
+
+setup()
