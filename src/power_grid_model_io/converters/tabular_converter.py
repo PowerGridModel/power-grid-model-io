580cbc8cf9d3788aab89240ce4443e8cd6c5d21b
Bram Stoeller <bram.stoeller@alliander.com>
2022-10-19 17:14:39 +0200

    Disable unexpected-keyword-arg for np.concatenate dtype argument
    Signed-off-by: Bram Stoeller <bram.stoeller@alliander.com>

--- src/power_grid_model_io/converters/tabular_converter.py
+++ src/power_grid_model_io/converters/tabular_converter.py
@@ -292,6 +292,7 @@ class TabularConverter(BaseConverter[TabularData]):
 
             # If there are multiple arrays, concatenate them
             elif len(data_set) > 1:
+                # pylint: disable=unexpected-keyword-arg
                 merged[component_name] = np.concatenate(data_set, dtype=data_set[0].dtype)
 
         return merged
