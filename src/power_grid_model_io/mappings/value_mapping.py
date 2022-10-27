363e97bf5cbda62be5f6ae449e77ccc676e0a742
Bram Stoeller <bram.stoeller@alliander.com>
2022-10-27 17:18:27 +0200

    Add multiplier support
    Signed-off-by: Bram Stoeller <bram.stoeller@alliander.com>

--- src/power_grid_model_io/mappings/value_mapping.py
+++ src/power_grid_model_io/mappings/value_mapping.py
@@ -5,46 +5,31 @@
 Value substitution helper class
 """
 
-import re
-from functools import lru_cache
 from typing import Dict, Optional, Union
 
-import structlog
+from power_grid_model_io.mappings.field_mapping import FieldMapping
 
 Value = Union[int, float, str, bool]
 
 #             attr      key    value
 Values = Dict[str, Dict[Value, Value]]
-ValuesRe = Dict[re.Pattern, Dict[Value, Value]]
 
 
 # pylint: disable=too-few-public-methods
-class ValueMapping:
+class ValueMapping(FieldMapping[Dict[Value, Value]]):
     """
     Value substitution helper class
     """
 
     def __init__(self, mapping: Optional[Values] = None):
-        self._log = structlog.get_logger(type(self).__name__)
-        self._values: Values = mapping or {}
-        self._values_re: ValuesRe = {re.compile(pattern): value for pattern, value in self._values.items()}
+        super().__init__(mapping=mapping)
         if mapping is not None:
             self._log.debug(
-                "Set value mapping", n_fields=len(mapping), n_mappings=sum(len(m) for m in mapping.values())
+                "Set value mapping", n_attributes=len(mapping), n_mappings=sum(len(m) for m in mapping.values())
             )
 
-    @lru_cache
-    def get_substitutions(self, field: str) -> Dict[Value, Value]:
+    def get_substitutions(self, attr: str, table: Optional[str] = None) -> Dict[Value, Value]:
         """
-        Find the substitutions for a given field.
+        Find the substitutions for a given attribute.
         """
-
-        # First check if there is an exact match
-        if field in self._values:
-            return self._values[field]
-
-        # Otherwise, use the values as regular expressions
-        for pattern in self._values_re.keys():
-            if pattern.fullmatch(field):
-                return self._values_re[pattern]
-        raise KeyError(field)
+        return self._get_mapping(attr=attr, table=table)
