0fb44289137c4e6dc23c59aa12fcb4992f7a7ea7
Peter Salemink <peter.salemink@alliander.com>
2022-10-31 16:42:25 +0100

    vision converter id_lookup
    Signed-off-by: Peter Salemink <peter.salemink@alliander.com>

--- tests/unit/converters/test_vision_excel_converter.py
+++ tests/unit/converters/test_vision_excel_converter.py
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 from pathlib import Path
+
+import pandas as pd
 import pytest
 from unittest.mock import patch
 
@@ -34,5 +36,27 @@ def test_initialization():
         # MockFileStore.assert_called_once()
 
 
-def test_converter__id_lookup():
-    pass
+def test_converter__id_lookup(converter: VisionExcelConverter):
+    row_1 = pd.Series([4.0, 5.0, 6.0], index=["a.b", "c.d.e", "a.c"])
+    row_2 = pd.Series([1.0, 5.0, 6.0], index=["a.b", "c.d.e", "a.c"])  # change in values
+    row_3 = pd.Series([4.0, 5.0, 6.0], index=["z.b", "c.d.e", "a.c"])  # change in index
+
+    assert converter._id_lookup(component="node", row=row_1) == 0
+    assert converter._id_lookup(component="node", row=row_2) == 1
+    assert converter._id_lookup(component="node", row=row_3) == 2
+    assert converter._id_lookup(component="node", row=row_1) == 0
+
+    assert converter._lookup._keys == {
+        "node:b=4.0,c=6.0,e=5.0": 0,
+        "node:b=1.0,c=6.0,e=5.0": 1,
+        "node:c=6.0,e=5.0,b=4.0": 2
+    }
+    assert converter._lookup._items[0] == {
+        "component": "node", "row": {"b": 4.0, "c": 6.0, "e": 5.0}
+    }
+    assert converter._lookup._items[1] == {
+        "component": "node", "row": {"b": 1.0, "c": 6.0, "e": 5.0}
+    }
+    assert converter._lookup._items[2] == {
+        "component": "node", "row": {"c": 6.0, "e": 5.0, "b": 4.0}
+    }
