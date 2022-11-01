f619468061cefc2e5c8b543b01009771ef7ec197
Peter Salemink <peter.salemink@alliander.com>
2022-11-01 10:37:53 +0100

    clean up
    Signed-off-by: Peter Salemink <peter.salemink@alliander.com>

--- tests/unit/converters/test_vision_excel_converter.py
+++ tests/unit/converters/test_vision_excel_converter.py
@@ -1,15 +1,12 @@
 # SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model IO project <dynamic.grid.calculation@alliander.com>
 #
 # SPDX-License-Identifier: MPL-2.0
-from pathlib import Path
 from unittest.mock import patch
 
 import pandas as pd
 import pytest
 
 from power_grid_model_io.converters.vision_excel_converter import VisionExcelConverter
-
-MAPPING_FILE = Path(__file__).parent / "test_data/mapping.yaml"
 
 
 @pytest.fixture
