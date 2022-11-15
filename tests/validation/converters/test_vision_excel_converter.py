e54698972a5c7625b0d0929224cdb928f14e0047
Bram Stoeller <bram.stoeller@alliander.com>
2022-11-15 16:50:51 +0100

    Restructure the validation test
    Signed-off-by: Bram Stoeller <bram.stoeller@alliander.com>

--- tests/validation/converters/test_vision_excel_converter.py
+++ tests/validation/converters/test_vision_excel_converter.py
@@ -2,67 +2,66 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-import json
+from functools import lru_cache
 from pathlib import Path
-from typing import Generator
 
-import numpy as np
 import pandas as pd
 import pytest
+from power_grid_model.data_types import SingleDataset
 from power_grid_model.utils import import_json_data
 
 from power_grid_model_io.converters.vision_excel_converter import VisionExcelConverter
 
+from ..utils import component_attributes, select_values
+
 DATA_PATH = Path(__file__).parents[2] / "data" / "vision"
 
 
-def available_attributes() -> Generator[pytest.param, None, None]:
-    # Read the excel file and do the conversion (extra info won't be used)
-    actual, _extra_info = VisionExcelConverter(DATA_PATH / "vision_validation.xlsx").load_input_data()
+@lru_cache
+def load_actual() -> SingleDataset:
+    """
+    Read the excel file and do the conversion (extra info won't be used, for now)
+    """
+    actual_data, _extra_info = VisionExcelConverter(DATA_PATH / "vision_validation.xlsx").load_input_data()
+    return actual_data
 
-    # Read the json file (extra info is currently not tested and therefore not allowed)
+
+@lru_cache
+def load_expected() -> SingleDataset:
+    """
+    Read the json file (extra info is currently not tested and therefore not allowed)
+    """
     try:
-        expected = import_json_data(DATA_PATH / "vision_validation.json", data_type="input", ignore_extra=False)
+        expected_data = import_json_data(DATA_PATH / "vision_validation.json", data_type="input", ignore_extra=False)
     except ValueError as ex:
         if "ignore_extra=True" in str(ex):
             raise ValueError("Extra info is currently not tested and therefore not allowed") from ex
         else:
             raise
-
-    # Read the json file again (only the structure is used, i.e. the component names and attribute name)
-    with (DATA_PATH / "vision_validation.json").open("r") as json_file:
-        structure = json.load(json_file)
-
-    # Loop over all components in the validation file (in alphabetical order)
-    for component, objects in sorted(structure.items(), key=lambda x: x[0]):
-
-        # List the attributes
-        attributes = set().union(*(set(obj.keys()) for obj in objects))
-
-        # Loop over all attributes that exist both in the validation file
-        for attribute in sorted(attributes):
-            # Create an index series for both the actual data and the expected data
-            actual_values = pd.Series(actual[component][attribute], index=actual[component]["id"]).sort_index()
-            expected_values = pd.Series(expected[component][attribute], index=expected[component]["id"]).sort_index()
-
-            # Create a selection mask, to select only non-NaN values in the validation file
-            if expected_values.dtype == np.int8:
-                mask = expected_values != -(2**7)
-            elif expected_values.dtype == np.int32:
-                mask = expected_values != -(2**31)
-            elif expected_values.dtype == np.int64:
-                mask = expected_values != -(2**63)
-            else:
-                mask = ~pd.isna(expected_values)
-
-            # Use only the actual_values for which we have expected values
-            actual_values = actual_values[expected_values.index][mask]
-            expected_values = expected_values[mask]
-
-            # Yield the result as a param instance
-            yield pytest.param(actual_values, expected_values, id=f"{component}-{attribute}")
+    return expected_data
 
 
-@pytest.mark.parametrize(("actual", "expected"), available_attributes())
-def test_input_data(actual: pd.Series, expected: pd.Series):
+def test_input_data():
+    """
+    Unit test to preload the expected and actual data
+    """
+    # Arrange
+    expected = load_expected()
+
+    # Act
+    actual = load_actual()
+
+    # Assert
+    assert len(expected) <= len(actual)
+
+
+@pytest.mark.parametrize(("component", "attribute"), component_attributes(DATA_PATH / "vision_validation.json"))
+def test_attributes(component: str, attribute: str):
+    """
+    For each attribute, check if the actual values are consistent with the expected values
+    """
+    # Arrange / Act
+    actual, expected = select_values(load_actual(), load_expected(), component, attribute)
+
+    # Assert
     pd.testing.assert_series_equal(actual, expected)
