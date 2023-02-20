108f367185c6a84c8f6a4ae64a4b4bcedb623ddd
Nitish Bharambe <nitish.bharambe@alliander.com>
2023-02-20 10:07:17 +0100

    add column names
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- src/power_grid_model_io/converters/pandapower_converter.py
+++ src/power_grid_model_io/converters/pandapower_converter.py
@@ -1347,7 +1347,8 @@ class PandaPowerConverter(BaseConverter[PandaPowerData]):
         all_i_df = pd.concat(
             [
                 join_currents(table, bus_name, i_name)
-                if not rest_switches_absent[table] else pd.DataFrame(columns=["bus", "element", "et","i_ka"])
+                if not rest_switches_absent[table]
+                else pd.DataFrame(columns=["bus", "element", "et", "i_ka"])
                 for table, attr_names in switch_attrs.items()
                 for bus_name, i_name in attr_names.items()
             ]
