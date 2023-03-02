8668e8a38c9daa5a164c7d70401cdd6928e880cf
Bram Stoeller <bram.stoeller@alliander.com>
2023-03-02 15:30:02 +0100

    File downloader and extractor utility
    Signed-off-by: Bram Stoeller <bram.stoeller@alliander.com>

--- tests/utils.py
+++ tests/utils.py
@@ -268,3 +268,14 @@ class MockExcelFile:
 
     def parse(self, sheet_name: str, **_kwargs) -> pd.DataFrame:
         return self.data[sheet_name]
+
+
+class MockTqdm:
+    """To use: for x in tqdm(iterable)"""
+
+    def __init__(self, iterable=None, **kwargs):
+        self.iterable = iterable
+
+    def __iter__(self):
+        for item in self.iterable:
+            yield item
