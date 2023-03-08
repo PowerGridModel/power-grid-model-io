4adac294a170d98f21745cff84a2a00364680fca
Bram Stoeller <bram.stoeller@alliander.com>
2023-03-08 13:44:56 +0100

    Fix some mypy issues
    Signed-off-by: Bram Stoeller <bram.stoeller@alliander.com>

--- src/power_grid_model_io/converters/pandapower_converter.py
+++ src/power_grid_model_io/converters/pandapower_converter.py
@@ -2320,7 +2320,7 @@ class PandaPowerConverter(BaseConverter[PandaPowerData]):
         attr_data = pp_component_data[attribute]
 
         # If any of the attribute values are missing, and a default is supplied, fill the nans with the default value
-        nan_values = np.equal(attr_data, None) if attr_data.dtype is np.dtype("O") else np.isnan(attr_data)  # type: ignore
+        nan_values = np.equal(attr_data, np.array([None])) if attr_data.dtype is np.dtype("O") else np.isnan(attr_data)
         if any(nan_values):
             attr_data = attr_data.fillna(value=default, inplace=False)
 
