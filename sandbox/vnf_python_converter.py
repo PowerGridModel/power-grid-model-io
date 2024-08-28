d09c7422340149f27012da05a06830264aa04719
petersalemink95 <peter.salemink95@gmail.com>
2024-08-28 16:52:13 +0200

    convert cables
    Signed-off-by: petersalemink95 <peter.salemink95@gmail.com>

--- sandbox/vnf_python_converter.py
+++ sandbox/vnf_python_converter.py
@@ -16,7 +16,7 @@ mapping = file_path / "vnf_mapping.yaml"
 def parse_vnf(file_path: str|Path) -> pd.DataFrame:
     with open(file_path, "r") as file:
         file = file.read()
-        components = ["NODE", "LINE", "SOURCE", "LOAD"]
+        components = ["NODE", "LINE", "SOURCE", "LOAD", "CABLE"]
         component_dict = {}
         for component in components:
             component_dict[component] = parse_component(component, file)
@@ -42,6 +42,8 @@ def parse_component(component: str, file: str) -> pd.DataFrame:
                     component_df = parse_source(matches[0])
                 case "LOAD":
                     component_df = parse_load(matches[0])
+                case "CABLE":
+                    component_df = parse_cable(matches[0])
                 case _:
                     raise NotImplementedError(f"Component {component} not implemented.")
         return component_df
@@ -73,7 +75,7 @@ def parse_line(vnf_txt: str) -> pd.DataFrame:
             switch_state2 = re.search(r"SwitchState2:(\d)", line).group(1)
             line_general.append([guid, node1, node2, switch_state1, switch_state2])
         if line.startswith('#LinePart'):
-            # TODO check if a cable can have multiple line parts
+            # TODO check if a line can have multiple line parts
             r = re.search(r"R:(\d+(\.\d+)?)", line).group(1)
             x = re.search(r"X:(\d+(\.\d+)?)", line).group(1)
             c = re.search(r"C:(\d+(\.\d+)?)", line).group(1)
@@ -91,6 +93,38 @@ def parse_line(vnf_txt: str) -> pd.DataFrame:
     lines_list = [line_general[i] + line_part[i] for i in range(len(line_general))]
     lines_df = pd.DataFrame(lines_list, columns=["GUID", "Node1", "Node2", "SwitchState1", "SwitchState2", "R", "X", "C", "R0", "X0", "C0", "Inom1", "Inom2", "Inom3", "TR", "Length"])
     return lines_df
+
+
+def parse_cable(vnf_txt: str) -> pd.DataFrame:
+    cable_general = []
+    cable_part = []
+    lines = vnf_txt.strip().split("\n")
+    for line in lines:
+        if line.startswith('#General'):
+            guid = re.search(r"GUID:'{(.*?)}'", line).group(1)
+            node1 = re.search(r"Node1:'{(.*?)}'", line).group(1)
+            node2 = re.search(r"Node2:'{(.*?)}'", line).group(1)
+            switch_state1 = re.search(r"SwitchState1:(\d)", line).group(1)
+            switch_state2 = re.search(r"SwitchState2:(\d)", line).group(1)
+            cable_general.append([guid, node1, node2, switch_state1, switch_state2])
+        if line.startswith('#CableType'):
+            # TODO check if a cable can have multiple cable parts
+            r = re.search(r"R:(\d+(\.\d+)?)", line).group(1)
+            x = re.search(r"X:(\d+(\.\d+)?)", line).group(1)
+            c = re.search(r"C:(\d+(\.\d+)?)", line).group(1)
+            r0 = re.search(r"R0:(\d+(\.\d+)?)", line).group(1)
+            x0 = re.search(r"X0:(\d+(\.\d+)?)", line).group(1)
+            c0 = re.search(r"C0:(\d+(\.\d+)?)", line).group(1)
+            # TODO check with p2p which 1nom is needed 0/1/2/3
+            inom0 = re.search(r"Inom0:(\d+(\.\d+)?)", line).group(1)
+            inom1 = re.search(r"Inom1:(\d+(\.\d+)?)", line).group(1)
+            inom2 = re.search(r"Inom2:(\d+(\.\d+)?)", line).group(1)
+            inom3 = re.search(r"Inom3:(\d+(\.\d+)?)", line).group(1)
+            cable_part.append([r, x, c, r0, x0, c0, inom0, inom1, inom2, inom3])
+    assert len(cable_general) == len(cable_part), "Number of line general and line part do not match."
+    cable_list = [cable_general[i] + cable_part[i] for i in range(len(cable_general))]
+    cable_df = pd.DataFrame(cable_list, columns=["GUID", "Node1", "Node2", "SwitchState1", "SwitchState2", "R", "X", "C", "R0", "X0", "C0", "Inom0", "Inom1", "Inom2", "Inom3"])
+    return cable_df
 
 
 def parse_source(vnf_txt: str) -> pd.DataFrame:
@@ -122,7 +156,7 @@ def parse_load(vnf_txt: str) -> pd.DataFrame:
             q = re.search(r"Q:(\d+(\.\d+)?)", line).group(1)
             loads_list.append([guid, node, switch_state, p, q])
     loads_df = pd.DataFrame(loads_list, columns=["GUID", "Node", "SwitchState", "P", "Q"])
-    # TODO: how to include "gelijktijdigheid"? See excel converter yaml -> can be in NODE
+    # TODO: how to include "gelijktijdigheid / simultanity factor"? See excel converter yaml -> can be in NODE
     # TODO: how to inlcude "load behaviour"? See excel converter yaml
     # TODO: how to include the right multiplier?
     return loads_df
@@ -149,7 +183,7 @@ for comp, df in component_dict.items():
     print(df)
     print()
 
-component_data = TabularData(NODE=component_dict["NODE"], LINE=component_dict["LINE"], SOURCE=component_dict["SOURCE"], LOAD=component_dict["LOAD"])
+component_data = TabularData(NODE=component_dict["NODE"], LINE=component_dict["LINE"], SOURCE=component_dict["SOURCE"], LOAD=component_dict["LOAD"], CABLE=component_dict["CABLE"])
 converter = TabularConverter(mapping_file=mapping)
 input_data, extra_info = converter.load_input_data(data=component_data)
 print(input_data)
@@ -158,3 +192,4 @@ model = PowerGridModel(input_data)
 result = model.calculate_power_flow()
 print("======== Result ========")
 print(result['node'])
+print(result['line'])
