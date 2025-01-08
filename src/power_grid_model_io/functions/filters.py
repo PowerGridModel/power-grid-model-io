e9911cc6bd05a33909ba67d53f90a339f6ea2b74
Santiago Figueroa Manrique <figueroa1395@gmail.com>
2025-01-08 09:57:13 +0100

    address comment
    Signed-off-by: Santiago Figueroa Manrique <figueroa1395@gmail.com>

--- src/power_grid_model_io/functions/filters.py
+++ src/power_grid_model_io/functions/filters.py
@@ -31,8 +31,9 @@ def exclude_value(row: pd.Series, col: str, value: Union[float, str]) -> bool:
     if col not in row:
         raise ValueError(f"The column: '{col}' cannot be found for the filter")
     result = row[col] != value
+    # Sonar cloud false positive: result can be a pd.Series of bool
     if isinstance(result, pd.Series):  # sonar-ignore: S2583
-        return result.item()  # sonar-ignore: S2583
+        return result.item()
     return result
 
 
