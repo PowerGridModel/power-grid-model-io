f656c03407713f896f8eef8e2a1b1db3178d18e2
furqan463 <ahmadfurqanc@gmail.com>
2026-09-17 15:03:13 +0500

    Add networks module and data files
    Signed-off-by: furqan463 <ahmadfurqanc@gmail.com>

---
+++ src/power_grid_model_io/networks/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+#
+# SPDX-License-Identifier: MPL-2.0
+"""
+Networks
+"""
+
+from power_grid_model_io.networks.ieee_test_networks import ieee_four_bus
+
+__all__ = ["ieee_four_bus"]
